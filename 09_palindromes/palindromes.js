a44ae80effc9e6a2d384019f13a4fa5e0a3ffc59
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2024-12-13 22:44:32 +0800

    fix: complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const cleanString = string.toLowerCase().replace(/[^\w]/g, "");
+    const reverseString = cleanString.split("").reverse().join("");
+    return (cleanString === reverseString);
 };
-
+console.log(palindromes('ZZZZ car, a man, a maracaz.'));
 // Do not edit below this line
 module.exports = palindromes;
