28c4b12667f5517ed404f9e65798c7ec52169c7f
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2024-12-13 21:53:10 +0800

    fix: complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	const summedArray = array.reduce((total, item) => {
+    return total + item;
+  }, 0);
+  return summedArray;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((multiplied, item) => multiplied * item);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	const numberedArray  = [];
+  if (number === 0) {
+    return 1;
+  } else {
+    for (let i = 1; i <= number; i++) {
+      if (i <= number) {
+        numberedArray.push(i);
+      };
+    };
+  };
+  return numberedArray.reduce((calculation, item) => {
+    return calculation * item;
+  });
 };
 
 // Do not edit below this line
